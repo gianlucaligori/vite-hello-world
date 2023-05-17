0c63e5542639ee795c6202872c48e9ad49fa394c
gianlucaligori <gianluca.ligori.95@gmail.com>
2023-05-17 17:23:37 +0200

    add AppTitle and bonus

---
+++ src/AppTitle.vue
@@ -0,0 +1,19 @@
+<script>
+import AppHero from './components/AppHero.vue'
+
+export default {
+  components: {
+    AppHero
+  }
+}
+
+</script>
+
+<template>
+<h1>La mia prima app con Vite</h1>
+<AppHero/>
+</template>
+
+<style scoped>
+
+</style> 
---
+++ src/components/AppHero.vue
@@ -0,0 +1,15 @@
+<script>
+
+</script>
+
+<template>
+    <div class="jumbotron">
+
+        <h1 class="componente-app-hero" >Componente App hero</h1>
+
+    </div>
+</template>
+
+<style scoped>
+
+</style> 
